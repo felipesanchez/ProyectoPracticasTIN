4d92d78f15e3e8da06b36701759cc8dbed378ae1
Dani Peralta <dani.peralta.de@gmail.com>
2014-05-28 19:29:03 +0200

    Todas las dimensiones. Funcionando perfectamente

--- SpoonJob/DatawarehouseScripts/DatawarehouseTIN.sql
+++ SpoonJob/DatawarehouseScripts/DatawarehouseTIN.sql
@@ -2,15 +2,15 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `ProyectoTIN` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `ProyectoTIN` ;
+CREATE SCHEMA IF NOT EXISTS `proyectotin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `proyectotin` ;
 
 -- -----------------------------------------------------
--- Table `ProyectoTIN`.`project_sources`
+-- Table `proyectotin`.`project_sources`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ProyectoTIN`.`project_sources` ;
+DROP TABLE IF EXISTS `proyectotin`.`project_sources` ;
 
-CREATE TABLE IF NOT EXISTS `ProyectoTIN`.`project_sources` (
+CREATE TABLE IF NOT EXISTS `proyectotin`.`project_sources` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `project_source_name` VARCHAR(45) NULL,
   `project_source_url` VARCHAR(255) NULL,
@@ -19,11 +19,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ProyectoTIN`.`project_intended_audiences`
+-- Table `proyectotin`.`project_intended_audiences`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ProyectoTIN`.`project_intended_audiences` ;
+DROP TABLE IF EXISTS `proyectotin`.`project_intended_audiences` ;
 
-CREATE TABLE IF NOT EXISTS `ProyectoTIN`.`project_intended_audiences` (
+CREATE TABLE IF NOT EXISTS `proyectotin`.`project_intended_audiences` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `code` VARCHAR(45) NULL,
   `description` VARCHAR(255) NULL,
@@ -32,11 +32,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ProyectoTIN`.`project_programming_languages`
+-- Table `proyectotin`.`project_programming_languages`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ProyectoTIN`.`project_programming_languages` ;
+DROP TABLE IF EXISTS `proyectotin`.`project_programming_languages` ;
 
-CREATE TABLE IF NOT EXISTS `ProyectoTIN`.`project_programming_languages` (
+CREATE TABLE IF NOT EXISTS `proyectotin`.`project_programming_languages` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `code` VARCHAR(45) NULL,
   `description` VARCHAR(255) NULL,
@@ -45,11 +45,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ProyectoTIN`.`project_date`
+-- Table `proyectotin`.`project_date`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ProyectoTIN`.`project_date` ;
+DROP TABLE IF EXISTS `proyectotin`.`project_date` ;
 
-CREATE TABLE IF NOT EXISTS `ProyectoTIN`.`project_date` (
+CREATE TABLE IF NOT EXISTS `proyectotin`.`project_date` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `date_registered` datetime NULL,
   `day` INT NULL,
@@ -60,11 +60,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ProyectoTIN`.`project_licenses`
+-- Table `proyectotin`.`project_licenses`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ProyectoTIN`.`project_licenses` ;
+DROP TABLE IF EXISTS `proyectotin`.`project_licenses` ;
 
-CREATE TABLE IF NOT EXISTS `ProyectoTIN`.`project_licenses` (
+CREATE TABLE IF NOT EXISTS `proyectotin`.`project_licenses` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `code` VARCHAR(45) NULL,
   `description` VARCHAR(255) NULL,
@@ -73,11 +73,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ProyectoTIN`.`project_operating_systems`
+-- Table `proyectotin`.`project_operating_systems`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ProyectoTIN`.`project_operating_systems` ;
+DROP TABLE IF EXISTS `proyectotin`.`project_operating_systems` ;
 
-CREATE TABLE IF NOT EXISTS `ProyectoTIN`.`project_operating_systems` (
+CREATE TABLE IF NOT EXISTS `proyectotin`.`project_operating_systems` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `code` VARCHAR(45) NULL,
   `description` VARCHAR(255) NULL,
@@ -86,11 +86,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ProyectoTIN`.`projects`
+-- Table `proyectotin`.`projects`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ProyectoTIN`.`projects` ;
+DROP TABLE IF EXISTS `proyectotin`.`projects` ;
 
-CREATE TABLE IF NOT EXISTS `ProyectoTIN`.`projects` (
+CREATE TABLE IF NOT EXISTS `proyectotin`.`projects` (
   `project_id` INT NOT NULL AUTO_INCREMENT,
   `project_long_name` VARCHAR(45) NULL,
   `developers_count` INT NULL,
@@ -110,32 +110,32 @@ CREATE TABLE IF NOT EXISTS `ProyectoTIN`.`projects` (
   INDEX `fk_projects_project_operating_systems1_idx` (`project_operating_systems_id` ASC),
   CONSTRAINT `fk_projects_project_date`
     FOREIGN KEY (`project_date_id`)
-    REFERENCES `ProyectoTIN`.`project_date` (`id`)
+    REFERENCES `proyectotin`.`project_date` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_projects_project_sources1`
     FOREIGN KEY (`project_sources_id`)
-    REFERENCES `ProyectoTIN`.`project_sources` (`id`)
+    REFERENCES `proyectotin`.`project_sources` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_projects_project_licenses1`
     FOREIGN KEY (`project_licenses_id`)
-    REFERENCES `ProyectoTIN`.`project_licenses` (`id`)
+    REFERENCES `proyectotin`.`project_licenses` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_projects_project_intended_audiences1`
     FOREIGN KEY (`project_intended_audiences_id`)
-    REFERENCES `ProyectoTIN`.`project_intended_audiences` (`id`)
+    REFERENCES `proyectotin`.`project_intended_audiences` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_projects_project_programming_languages1`
     FOREIGN KEY (`project_programming_languages_id`)
-    REFERENCES `ProyectoTIN`.`project_programming_languages` (`id`)
+    REFERENCES `proyectotin`.`project_programming_languages` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_projects_project_operating_systems1`
     FOREIGN KEY (`project_operating_systems_id`)
-    REFERENCES `ProyectoTIN`.`project_operating_systems` (`id`)
+    REFERENCES `proyectotin`.`project_operating_systems` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
